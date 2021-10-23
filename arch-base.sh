a189f08265eb49ba8b9902a3568b57593877cff4
alcrmel <68015943+alcrmel@users.noreply.github.com>
2021-10-23 21:55:04 +0800

    Update arch-base.sh

--- arch-base.sh
+++ arch-base.sh
@@ -41,7 +41,7 @@ printf "[Done] Changing password of root : chpasswd"
 #    nfs-utils - Network file system utilities
 # # You can remove the tlp package if you are installing on a desktop or vm
 printf "[Start] Downloading required packages : pacman -S --noconfirm"
-pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 printf "[Done] Downloading required packages : pacman -S --noconfirm"
 
 # pacman -S --noconfirm xf86-video-amdgpu
